471f1199915554dd934fe97b1a0b1481f1d1542e
Gajanand <gajanand@Gajanands-MacBook-Pro.local>
2024-12-25 18:07:21 +0530

    authentication done

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,46 @@
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
+
+function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMesssage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://expense-tracker-react-na-d46ce-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setFetchedMesssage(response.data);
+      });
+  }, [token]);
+
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 32,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
